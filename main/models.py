84a5c86d928f11b0083bb72fc4127864562d44a1
AdamMoh <xlkartu12345@gmail.com>
2023-09-07 22:04:13 +0700

    update models and migrations 20230907

--- main/models.py
+++ main/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=19, decimal_places=2)
     category = models.TextField()
     description = models.TextField()
